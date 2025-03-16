a1a64fe580acfab84426c14a21673320a7c5e790
Kiran Anand <liteninkiran@hotmail.com>
2025-03-16 14:18:04 +0000

    Add angular material and home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'd3-project';
+    title = 'd3-project';
 }
