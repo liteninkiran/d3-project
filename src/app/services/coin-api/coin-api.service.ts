f7075c0188688b551879aba63bc5f3e54b1cc02d
Kiran Anand <liteninkiran@hotmail.com>
2025-03-16 17:15:31 +0000

    Add API key

--- src/app/services/coin-api/coin-api.service.ts
+++ src/app/services/coin-api/coin-api.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Asset } from 'src/app/interfaces/coin-api/asset';
 
 @Injectable()
@@ -9,7 +9,8 @@ export class CoinApiService {
     constructor(private http: HttpClient) { }
 
     public loadDataFromApi(): Observable<Asset[]> {
-        const url = 'https://rest.coinapi.io/v1/assets?apikey=';
+        const key = process.env['API_KEY'];
+        const url = `https://rest.coinapi.io/v1/assets?apikey=${key}`;
         return this.http.get<Asset[]>(url);
     }
 }
