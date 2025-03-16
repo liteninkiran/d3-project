03d301a99e2789ccfabe0d2f6513c4c7aad903ad
Kiran Anand <liteninkiran@hotmail.com>
2025-03-16 16:26:00 +0000

    Add CoinAPI component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CoinApiComponent } from './pages/coin-api/coin-api.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
+    },
+    {
+        path: 'coin-api',
+        component: CoinApiComponent,
     }
 ];
 
---
+++ src/app/pages/coin-api/coin-api.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-coin-api',
+    templateUrl: './coin-api.component.html',
+    styleUrls: ['./coin-api.component.scss'],
+})
+export class CoinApiComponent {
+
+}
