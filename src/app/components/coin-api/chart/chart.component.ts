baa624f5d6fa4656cb5576610661f2b4d85e205f
Kiran Anand <liteninkiran@hotmail.com>
2025-03-18 20:42:58 +0000

    Get data

--- src/app/components/coin-api/chart/chart.component.ts
+++ src/app/components/coin-api/chart/chart.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { Observable } from 'rxjs';
 import { ExchangeRate } from 'src/app/interfaces/coin-api/exchange-rate';
 import { ExchangeRateOptions } from 'src/app/services/coin-api/coin-api.service';
+import { CoinApiStore } from 'src/app/stores/coin-api.store';
 
 @Component({
     selector: 'app-coin-api-chart',
@@ -14,11 +15,22 @@ export class ChartComponent implements OnInit, OnChanges {
 
     public data$: Observable<ExchangeRate[]> = new Observable();
 
+    constructor(
+        private readonly store: CoinApiStore,
+    ) { }
+
     public ngOnInit(): void {
-        // Get data
+        this.getData();
     }
 
     public ngOnChanges(changes: SimpleChanges): void {
-        // Get data
+        this.getData();
+    }
+
+    public getData() {
+        this.data$ = this.store.getExchangeRateData(this.options)
+        this.data$.subscribe(x => {
+            console.log(x);
+        });
     }
 }
