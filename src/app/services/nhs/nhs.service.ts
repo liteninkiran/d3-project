fbfbf0ad083e3062c145240c016030db92adfdaa
Kiran Anand <liteninkiran@hotmail.com>
2025-03-24 19:51:32 +0000

    Add service

--- src/app/services/nhs/nhs.service.ts
+++ src/app/services/nhs/nhs.service.ts
@@ -1,21 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Asset } from 'src/app/types/coin-api/asset';
-import { ExchangeRate } from 'src/app/types/coin-api/exchange-rate';
-import { TimePeriod } from 'src/app/types/coin-api/time-period';
+import { DatastoreSearch, DatastoreSearchSql } from 'src/app/types/nhs/epd';
 
-const baseUrl = 'https://rest.coinapi.io';
-const key = process.env['API_KEY'];
-
-export interface ExchangeRateOptions {
-    startTime: string;
-    endTime: string;
-    periodId: string;
-    limit: number;
-    assetIdBase: string;
-    assetIdQuote: string;
-}
+const baseUrl = 'https://opendata.nhsbsa.net/api/3/action/datastore_search';
 
 @Injectable({
     providedIn: 'root'
@@ -24,20 +12,13 @@ export class CoinApiService {
 
     constructor(private http: HttpClient) { }
 
-    public getAssets(): Observable<Asset[]> {
-        const url = `${baseUrl}/v1/assets?apikey=${key}`;
-        return this.http.get<Asset[]>(url);
+    public datastoreSearch(options: string): Observable<DatastoreSearch[]> {
+        const url = `${baseUrl}?${options}`;
+        return this.http.get<DatastoreSearch[]>(url);
     }
 
-    public getTimePeriods(): Observable<TimePeriod[]> {
-        const url = `${baseUrl}/v1/exchangerate/history/periods?apikey=${key}`;
-        return this.http.get<TimePeriod[]>(url);
-    }
-
-    public getExchangeRates(options: ExchangeRateOptions): Observable<ExchangeRate[]> {
-        const path = `${baseUrl}/v1/exchangerate/${options.assetIdBase}/${options.assetIdQuote}/history`;
-        const params = `apikey=${key}&period_id=${options.periodId}&time_start=${options.startTime}&time_end=${options.endTime}&limit=${options.limit}`;
-        const url = `${path}?${params}`;
-        return this.http.get<ExchangeRate[]>(url);
+    public datastoreSearchSql(sql: string): Observable<DatastoreSearchSql[]> {
+        const url = `${baseUrl}_sql?${sql}`;
+        return this.http.get<DatastoreSearchSql[]>(url);
     }
 }
