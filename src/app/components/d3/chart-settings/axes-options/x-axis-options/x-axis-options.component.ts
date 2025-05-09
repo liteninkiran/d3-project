d93b75d9fb380ff033ae7e4df3c2c65fc6cec01f
Kiran Anand <liteninkiran@hotmail.com>
2025-05-09 18:43:03 +0100

    Add blank components

---
+++ src/app/components/d3/chart-settings/axes-options/x-axis-options/x-axis-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-x-axis-options',
+    templateUrl: './x-axis-options.component.html',
+    styleUrls: ['./x-axis-options.component.scss'],
+})
+export class XAxisOptionsComponent {
+
+}
---
+++ src/app/components/d3/chart-settings/axes-options/y-axis-options/y-axis-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-y-axis-options',
+    templateUrl: './y-axis-options.component.html',
+    styleUrls: ['./y-axis-options.component.scss'],
+})
+export class YAxisOptionsComponent {
+
+}
